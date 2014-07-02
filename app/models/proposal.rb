607939bb960479dbffdd91eb28d700b14556c0f0
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-07-01 22:33:17 -0400

    proposal.ratify

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -8,17 +8,18 @@ class Proposal < ActiveRecord::Base
   mount_uploader :icon, ImageUploader
   
   def ratify
-    if votes.up_votes.size > members.size / 2 and members.size > 2
+    group = Group.find(group_id)
+    if votes.up_votes.size > group.members.size / 2 and group.members.size > 2
       update inactive: true
       case action
         when "icon_change"
-          Group.find(group_id).update(icon: icon)
+          group.update(icon: icon)
         when "name_change"
-          Group.find(group_id).update(name: submission)
+          group.update(name: submission)
         when "description_change"
-          Group.find(group_id).update(description: submission)
+          group.update(description: submission)
         when "request_to_join"
-          Group.find(group_id).members.create(user_id: user_id)
+          group.members.create(user_id: user_id)
       end
     end
   end
