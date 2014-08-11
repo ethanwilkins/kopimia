b8a21a1bf354e575ea04585db03c494fed030dd5
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-08-11 03:58:13 -0400

    micro_mgmt

---
+++ pull.sh
@@ -0,0 +1,11 @@
+cd /Users/ethanwilkins/Documents/GitHub/social
+
+git pull
+
+# rm -rf /Users/ethanwilkins/Documents/ruby/rails/social/*
+
+cp -r /Users/ethanwilkins/Documents/GitHub/social/ /Users/ethanwilkins/Documents/ruby/rails/social/
+
+cdrails
+
+cd social
