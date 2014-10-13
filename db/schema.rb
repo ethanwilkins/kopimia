68ea2bc8b0decabc24c508ea6864e1d39d900308
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-10-13 05:15:33 -0400

    more_darkness

--- db/schema.rb
+++ db/schema.rb
@@ -98,8 +98,17 @@ ActiveRecord::Schema.define(version: 20141011034525) do
     t.integer  "federation_id"
   end
 
-# Could not dump table "members" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "members", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.integer  "folder_id"
+    t.boolean  "active_contributor"
+    t.integer  "federation_id"
+    t.integer  "federated_group_id"
+    t.integer  "reputation"
+  end
 
   create_table "messages", force: true do |t|
     t.integer  "receiver"
