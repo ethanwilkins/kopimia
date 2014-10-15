77fb6017e6cfa3633e7731f6c3a7607f33705d77
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-10-15 16:38:42 -0400

    search_more

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def search
+    unless session[:more]
+      session[:page] = nil
+    end
+    session[:more] = nil
+    
+    # saves in session for pages
     session[:query] = params[:query]
     if session[:query]
       @query = session[:query]
@@ -8,18 +14,18 @@ class SearchController < ApplicationController
       @modules = CodeModule.where "name = ? OR name = ?", session[:query].capitalize, session[:query].downcase
       @tags = Hashtag.tagged(session[:query])
 
-      
+      # show a listing of groups for empty searches
       if session[:query] == ""
         @groups = Group.all
       elsif @users.empty? and @groups.empty? and @modules.empty? and @tags.empty?
         @no_results = "No results were found."
       end
 
-      @all_results = []
-      @users.any? { |user| @all_results << user }
-      @groups.any? { |group| @all_results << group }
-      @modules.any? { |_module| @all_results << _module }
-      @tags.any? { |tag| @all_results << tag }
+      @all_results = [] # collects results into one array
+      @users.each { |user| @all_results << user }
+      @groups.each { |group| @all_results << group }
+      @modules.each { |_module| @all_results << _module }
+      @tags.each { |tag| @all_results << tag }
       
       @results = @all_results.
         # drops first several posts if :feed_page
@@ -29,9 +35,5 @@ class SearchController < ApplicationController
     end
     # logs the visit with the contextual data
     Activity.log_action(current_user, request.remote_ip.to_s, "search", nil, @query)
-    unless session[:more]
-      session[:page] = nil
-    end
-    session[:more] = nil
   end
 end
