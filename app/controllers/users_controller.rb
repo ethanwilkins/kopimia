c57f014b5095e55e16b38b336038bc6bdd2ae88e
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-10-21 05:46:50 -0400

    connections_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def connections
     # page for people, groups, and federations of user
+    @user = User.find(params[:user_id])
   end
   
   def following
