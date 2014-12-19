9a0683048c140928518bfa9bc512db271a141108
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-12-19 02:18:28 -0500

    proposal_fields_and_user_action

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -15,21 +15,23 @@ class Proposal < ActiveRecord::Base
   def available_types
     types = [["Proposal type", nil]]
     case self.action.to_sym
-      when :icon_change
+      when :icon_change # change proposed icon for group
         types << ["Icon change", "proposal_icon_change"]
-      when :name_change
+      when :name_change # change proposed name for group
         types << ["Name change", "proposal_name_change"]
-      when :description_change
+      when :description_change # proposed description for group
         types << ["Description change", "proposal_description_change"]
-      when :add_module
+      when :add_module # change attributes for proposed module
         types << ["Change code or link", "proposal_module_code_change"]
         types << ["Change module name", "proposal_module_name_change"]
         types << ["Change module description", "proposal_module_description_change"]
-      when :req_to_join_federation
+      when :req_to_join_federation # propose to request to join a different federation
         types << ["Join different federation", "proposal_req_to_join_dif_federation"]
       when :federate_request
         types << ["Federate with different group", "proposal_federate_w_dif_group"]
         types << ["Change proposed federation name", "proposal_dif_federation_name"]
+        types << ["Change proposed federation name", "proposal_dif_federation_description"]
+        types << ["Change proposed federation icon", "proposal_dif_federation_icon"]
     end
     return types
   end
@@ -55,12 +57,12 @@ class Proposal < ActiveRecord::Base
           federated_federation.proposals.create action: "join_federation", why: why,
             user_id: user_id, federated_group_id: group_id
         when "federate_request" # requests other group to federate
-          Group.find(federated_group_id).proposals.create action: "federate",
-            icon: icon, item_name: item_name, federated_group_id: group_id
+          Group.find(federated_group_id).proposals.create action: "federate", icon: icon,
+            item_name: item_name, description: description, federated_group_id: group_id
         when "join_federation" # adds the group to the federation
           federation.members.create federated_group_id: federated_group_id
         when "federate" # other group ratifies and creates the federation
-          federation = Federation.create name: item_name, icon: icon, description: submission
+          federation = Federation.create name: item_name, icon: icon, description: description
           federation.members.create federated_group_id: federated_group_id
           federation.members.create federated_group_id: group_id
         when "disband"
